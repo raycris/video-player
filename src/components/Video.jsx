621b4d6644fdb05424aae2b5b9cb791bd7fc3ce3
Raycris Maldonado <raycris.maldonado@utreee.com>
2023-03-15 15:46:22 -0400

    btn working good

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,20 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-import styles from "./Video.module.css"
+import styles from "./Video.module.css";
 
-const Video = ({videoSelected}) => {
-  console.log(videoSelected);
+const Video = ({ videoSelected }) => {
+  const [autoPlay, setAutoPlay] = useState(true);
+  const [videoLoop, setVideoLoop] = useState(false);
+
+  const handleLoop = () => {
+    setVideoLoop(!videoLoop);
+    console.log(videoLoop);
+  };
+
+  const handleAutoPlay = () => {
+    setAutoPlay(!autoPlay);
+    console.log("autoplay: "+autoPlay);
+  };
+
+
+
   return (
     <div className={styles.container}>
       <video
-        loop
         controls
-        autoPlay
         muted
+        autoPlay={autoPlay}
+        loop={videoLoop}
         className={styles.videoCase}
         autostart="true"
         src={videoSelected}
       />
+      <div className={styles.btnContainer}>
+
+      <button onClick={handleLoop}>
+        {videoLoop ? "Stop loop" : "Play loop"}
+      </button>
+      <button onClick={handleAutoPlay}>
+        {autoPlay ? "Stop autoPlay" : "Auto play"}
+      </button>
+      </div>
     </div>
   );
 };
