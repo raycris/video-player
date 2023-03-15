1a361894596bc453bd4cd2c5c15675a75da4c046
Raycris Maldonado <raycris.maldonado@utreee.com>
2023-03-15 11:57:00 -0400

    main part

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+const videos = {
+  deer: "https://s3.amazonaws.com/codecademy-content/courses/React/react_video-fast.mp4",
+  cat: "https://s3.amazonaws.com/codecademy-content/courses/React/react_video-cute.mp4",
+  snail:
+    "https://s3.amazonaws.com/codecademy-content/courses/React/react_video-slow.mp4",
+  spider:
+    "https://s3.amazonaws.com/codecademy-content/courses/React/react_video-eek.mp4",
+};
+
+const videoNames = Object.keys(videos);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [videoSrc, setVideoSrc] = useState(videos.deer);
+
+  function onSelectVideo(video) {
+    const videoSelected = videos[video];
+    setVideoSrc(videoSelected);
+  }
 
   return (
-    <div >
-      <h1>Hola</h1>
+    <div>
+      <h1>Video Player</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
